009217095b9969f7e49caa628a33cb1b45978ac8
Lucia Ferencik <lferencikova@gmail.com>
2023-02-09 14:25:03 +0000

    Tidy up the rules

--- run.py
+++ run.py
@@ -12,15 +12,15 @@ Your goal is to move the disks from the first stack to the last stack.
 
 Rules:
 
-1. Only one shape maybe be moved at a time.
-2. Each move consists of taking the uppermost shape from one of the stacks
+1. Only one disk may be moved at a time.
+2. Each move consists of taking the uppermost disk from one of the stacks
    and placing it on top of another stack or on an empty rod.
-3. No shape may be placed on top of a disk that is smaller than itself.
+3. No disk may be placed on top of a disk that is smaller than itself.
 
 How to play:
 
-1. Indicate the stack from which you want to move the uppermost shape.
-2. Indicate the stack on which you want to place the uppermost shape.
+1. Indicate the stack from which you want to move the uppermost disk.
+2. Indicate the stack on which you want to place the chosen disk.
 
 Good luck!\n""")
 
